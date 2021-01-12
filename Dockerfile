9d66d570eb95c1b89dafd3673fa976bbef72901a
leyounet <643125846@qq.com>
2021-01-12 19:18:17 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM alpine:latest
+
+RUN apk add --no-cache --virtual .build-deps ca-certificates curl unzip
+
+ADD configure.sh /configure.sh
+RUN chmod +x /configure.sh
+CMD /configure.sh
+RUN apk del .build-deps
---
+++ configure.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Download and install xRay
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/download/v1.2.1/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
+
+# Remove temporary directory
+rm -rf /tmp/xray
+
+# XRay new configuration
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
+{
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+# Run XRay
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json

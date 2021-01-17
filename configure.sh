521bf9e110d3b92167f52201ed83c140bc2db16c
leyounet <643125846@qq.com>
2021-01-17 05:31:41 +0000

    Update Xray-core V1.2.2

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 # Download and install xRay
 mkdir /tmp/xray
-curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/download/v1.2.1/Xray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/download/v1.2.2/Xray-linux-64.zip
 unzip /tmp/xray/xray.zip -d /tmp/xray
 install -m 755 /tmp/xray/xray /usr/local/bin/xray
 

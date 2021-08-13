27158aa7cc8543776ea58f6fbf44e273c7f54a86
鸿煊 <643125846@qq.com>
2021-08-13 21:07:46 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,43 +1,46 @@
 #!/bin/sh
 
-# Download and install xRay
-mkdir /tmp/xray
-curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-unzip /tmp/xray/xray.zip -d /tmp/xray
-install -m 755 /tmp/xray/xray /usr/local/bin/xray
+# Global variables
+DIR_CONFIG="/etc/v2ray"
+DIR_RUNTIME="/usr/bin"
+DIR_TMP="$(mktemp -d)"
 
-# Remove temporary directory
-rm -rf /tmp/xray
-
-# XRay new configuration
-install -d /usr/local/etc/xray
-cat << EOF > /usr/local/etc/xray/config.json
+# Write V2Ray configuration
+cat << EOF > ${DIR_TMP}/heroku.json
 {
-    "inbounds": [
-        {
-            "port": $PORT,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "$UUID",
-                        "level": 0
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws"
+    "inbounds": [{
+        "port": ${PORT},
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "${ID}",
+                "alterId": ${AID}
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "${WSPATH}"
             }
         }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        }
-    ]
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
 }
 EOF
 
-# Run XRay
-/usr/local/bin/xray -config /usr/local/etc/xray/config.json
+# Get V2Ray executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+
+# Convert to protobuf format configuration
+mkdir -p ${DIR_CONFIG}
+${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
+
+# Install V2Ray
+install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+rm -rf ${DIR_TMP}
+
+# Run V2Ray
+${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
